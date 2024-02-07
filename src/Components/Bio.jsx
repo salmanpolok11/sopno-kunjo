6d0e0a1637eb29c27583156886add610ae8bd95d
salmanpolok11 <133965859+salmanpolok11@users.noreply.github.com>
2024-02-07 12:01:39 +0600

    added email

--- src/Components/Bio.jsx
+++ src/Components/Bio.jsx
@@ -1,7 +1,7 @@
-
+import { MdEmail } from "react-icons/md";
 const Bio = () => {
   return (
-    <div className=" w-full md:h-96  bg-blue-700">
+    <div className=" w-full md:h-96  bg-blue-700 p-2">
       <section className="md:flex justify-center ">
       <div>
           <img
@@ -11,10 +11,15 @@ const Bio = () => {
           />
         </div>
         <div>
-          <h1 className=" md:pt-28 text-white text-2xl  text-center pb-5  p-3">
+          <h1 className=" md:pt-28 text-white text-2xl  text-center   p-3">
           <span className=" text-3xl font-bold pb-2"> সালমান ইসলাম পলক </span> <br />
 স্বপ্নকুঞ্জের প্রতিষ্ঠাতা ।  লেখালেখিতে যার কেটেছে এক দশক <br />   তিনি একজন অখ্যাত লেখক। তার একাধিক লেখার মধ্যে <br /> জনপ্রিয় লেখা গল্পের নাম, "অনুতপ্ত" "মায়ের আঁচল" ।  
           </h1>
+           
+           <h1 className=" text-white flex items-center justify-center gap-2 text-2xl  pb-2">
+              <MdEmail/> <h1> salmanpolok224@gmail.com</h1>
+           </h1>
+
         </div>
         
       </section>
