1cf54cf957bc56dfa2b301a017154c9d27031525
salmanpolok11 <133965859+salmanpolok11@users.noreply.github.com>
2024-02-07 17:35:44 +0600

    added animetion

--- src/Page/Profile.jsx
+++ src/Page/Profile.jsx
@@ -2,7 +2,9 @@ const Profile = () => {
   return (
     <div>
       <secton>
-        <div className="reletive w-full md:h-[420px]  bg-blue-700">
+        <div className="reletive w-full md:h-[420px]  bg-blue-700"
+      
+        >
           <img
             className=" mx-auto pt-20 "
             src="https://i.ibb.co/x8sNz7Y/2205-w048-n004-118b-p1-118-removebg-preview.png"
@@ -10,7 +12,11 @@ const Profile = () => {
           />
         </div>
 
-        <div className=" mx-auto absolute top-48 ml-8 md:top-80 lg:bottom-20 w-80 h-60 border-2 md:ml-40  bg-red-600">
+        <div className=" mx-auto absolute top-48 ml-8 md:top-80 lg:bottom-20 w-80 h-60 border-2 md:ml-40  bg-red-600"
+        data-aos="fade-up"
+        data-aos-easing="linear"
+        data-aos-duration="900"
+        >
              
         </div>
 
