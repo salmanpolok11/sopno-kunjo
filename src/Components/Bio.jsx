0a490422956f20e8a9032ed7b9b91d132a1697a5
salmanpolok11 <133965859+salmanpolok11@users.noreply.github.com>
2024-03-12 00:13:39 +0600

    added fb page

--- src/Components/Bio.jsx
+++ src/Components/Bio.jsx
@@ -3,12 +3,11 @@ import { FaFacebookSquare } from "react-icons/fa";
 import { MdReadMore } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 
-
 const Bio = () => {
   return (
     <div className=" w-full md:h-96  bg-blue-700 p-2 mt-16">
       <section className="md:flex justify-center lg:gap-16 ">
-      <div>
+        <div>
           <img
             className=" md:w-[295px] mx-auto"
             src="https://i.ibb.co/cQh8Q3f/IMG-0765-removebg-preview.png"
@@ -16,27 +15,52 @@ const Bio = () => {
           />
         </div>
         <div className=" md:w-[500px] ">
-          <h1 className=" mt-3 md:pt-10 text-white md:text-2xl  text-center   ">
-          <span className=" text-2xl md:text-3xl font-bold pb-2"> সালমান ইসলাম পলক </span> <br />
-স্বপ্নকুঞ্জের প্রতিষ্ঠাতা ।  লেখালেখিতে যার কেটেছে এক দশক   তিনি একজন অখ্যাত লেখক। তার একাধিক লেখার মধ্যে  জনপ্রিয় লেখা গল্পের নাম, "অনুতপ্ত" "মায়ের আঁচল" ।  
+          <h1 className=" mt-3 md:pt-7 text-white md:text-2xl  text-center   ">
+            <span className=" text-2xl md:text-3xl font-bold pb-2">
+              {" "}
+              সালমান ইসলাম পলক{" "}
+            </span>{" "}
+            <br />
+            স্বপ্নকুঞ্জের প্রতিষ্ঠাতা । লেখালেখিতে যার কেটেছে এক দশক তিনি একজন
+            অখ্যাত লেখক। তার একাধিক লেখার মধ্যে জনপ্রিয় লেখা গল্পের নাম,
+            "অনুতপ্ত" "মায়ের আঁচল" ।
           </h1>
-           
+
           <h1 className=" text-white  text-xl md:text-2xl  ">
-              <a className="flex items-center justify-center  gap-2  hover:text-red-400" href="https://mail.google.com/mail/u/0/#inbox?compose=new ">
-              <MdEmail/>  salmanpolok224@gmail.com</a>
-           </h1>
-           <h1 className=" text-white text-xl md:text-2xl  ">
-              <a className="flex items-center  justify-center gap-2  hover:text-red-400" href="https://www.facebook.com/salmanpolok112">
-                 <FaFacebookSquare/> Salman Islam Polok</a>
-           </h1>
-            <NavLink to='/about'>
-            <h1  className=" text-white text-xl md:text-xl btn-outline btn-ghost w-32 px-3 mx-auto mt-2 rounded-md mb-3">
-              <button className="flex items-center  justify-center gap-2 text-center  hover:text-red-400" >
-              <MdReadMore />আরো   </button>
-           </h1>
-            </NavLink>
+            <a
+              className="flex items-center justify-center  gap-2  hover:text-red-400"
+              href="https://mail.google.com/mail/u/0/#inbox?compose=new "
+            >
+              <MdEmail /> salmanpolok224@gmail.com
+            </a>
+          </h1>
+          <h1 className=" text-white text-xl md:text-2xl  ">
+            <a
+              className="flex items-center  justify-center gap-2  hover:text-red-400"
+              href="https://www.facebook.com/salmanpolok112"
+            >
+              <FaFacebookSquare /> Salman Islam Polok
+            </a>
+          </h1>
+
+          <h1 className=" text-white text-xl md:text-2xl  ">
+              <a
+                className="flex items-center justify-center  gap-2  hover:text-red-400"
+                href="https://www.facebook.com/profile.php?id=61557035798428"
+              >
+                  <FaFacebookSquare />  Sopno Kunjo
+              </a>
+            </h1>
+
+          <NavLink to="/about">
+            <h1 className=" text-white text-xl md:text-xl btn-outline btn-ghost w-32 px-3 mx-auto mt-2 rounded-md mb-3">
+              <button className="flex items-center  justify-center gap-2 text-center  hover:text-red-400">
+                <MdReadMore />
+                আরো{" "}
+              </button>
+            </h1>
+          </NavLink>
         </div>
-        
       </section>
     </div>
   );
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,49 +6,68 @@ import { MdReadMore } from "react-icons/md";
 const Footer = () => {
   return (
     <div>
-   <footer className=" lg:space-x-20 mt-10 md:mt-20  mx-auto md:flex justify-center  bg-[#536BF6] text-black border">
-   <section className=" mt-6 ">
-      <img className=" w-32 mx-auto  md:ml-10"  src="https://i.ibb.co/0FqcghF/588-removebg-preview.png" alt="" />
-    <h1 className=" text-center font-semibold text-xl ">
-          “স্বপ্ন দেখ বিজয়ের, <br /> সাধনা করো{}
-            <span className=" text-white">শ্রেষ্ঠত্বের। </span>{}
+      <footer className=" lg:space-x-20 mt-10 md:mt-20  mx-auto md:flex justify-center  bg-[#536BF6] text-black border">
+        <section className=" mt-6 ">
+          <img
+            className=" w-32 mx-auto  md:ml-10"
+            src="https://i.ibb.co/0FqcghF/588-removebg-preview.png"
+            alt=""
+          />
+          <h1 className=" text-center font-semibold text-xl ">
+            “স্বপ্ন দেখ বিজয়ের, <br /> সাধনা করো{}
+            <span className=" text-white">শ্রেষ্ঠত্বের। </span>
+            {}
           </h1>
-        </section> 
+        </section>
 
-  <section>
-         <img className=" w-96   mx-auto" 
-         src="https://i.ibb.co/bgCh034/l-R0-K9l-facebook.gif" alt="" />
-  </section>
+        <section>
+          <img
+            className=" w-96   mx-auto"
+            src="https://i.ibb.co/bgCh034/l-R0-K9l-facebook.gif"
+            alt=""
+          />
+        </section>
 
-<section className=" text-white mt-7">
-    <h1 className=" text-center text-2xl">সোশাল মিডিয়া <hr /></h1>
-    <div className=" mt-4  text-center">
-    {/* */}
-    <h1 className=" text-white flex items-center justify-center gap-2  text-xl md:text-2xl  ">
-           <h1></h1>
-           </h1>
+        <section className=" text-white mt-7">
+          <h1 className=" text-center text-2xl">
+            সোশাল মিডিয়া <hr />
+          </h1>
+          <div className=" mt-4  text-center">
+            {/* */}
+            <h1 className=" text-white flex items-center justify-center gap-2  text-xl md:text-2xl  ">
+              <h1></h1>
+            </h1>
 
-           <h1 className=" text-white text-xl md:text-2xl  ">
-              <a className="flex items-center justify-center  gap-2  hover:text-red-400" href="https://mail.google.com/mail/u/0/#inbox?compose=new ">
-              <MdEmail/>  salmanpolok224@gmail.com</a>
-           </h1>
+            <h1 className=" text-white text-xl md:text-2xl  ">
+              <a
+                className="flex items-center justify-center  gap-2  hover:text-red-400"
+                href="https://www.facebook.com/profile.php?id=61557035798428"
+              >
+                  <FaFacebookSquare />  Sopno Kunjo
+              </a>
+            </h1>
 
-           <h1 className=" text-white text-xl md:text-2xl  ">
-              <a className="flex items-center justify-center  gap-2  hover:text-red-400" href="https://www.facebook.com/salmanpolok112">
-                 <FaFacebookSquare/> Salman Islam Polok</a>
-           </h1>
-           <NavLink to='/about'>
-            <h1  className=" text-white text-xl md:text-xl btn-outline btn-ghost w-32 px-3 mx-auto mt-2 rounded-md mb-3">
-              <button className="flex items-center  justify-center gap-2 text-center  hover:text-red-400" >
-              <MdReadMore />আরো   </button>
-           </h1>
+            <h1 className=" text-white text-xl md:text-2xl  ">
+              <a
+                className="flex items-center justify-center  gap-2  hover:text-red-400"
+                href="https://www.facebook.com/salmanpolok112"
+              >
+                <FaFacebookSquare /> Salman Islam Polok
+              </a>
+            </h1>
+            <NavLink to="/about">
+              <h1 className=" text-white text-xl md:text-xl btn-outline btn-ghost w-32 px-3 
+              mx-auto mt-2 rounded-md mb-3">
+                <button className="flex items-center  justify-center gap-2 text-center 
+                 hover:text-red-400">
+                  <MdReadMore />
+                  আরো{" "}
+                </button>
+              </h1>
             </NavLink>
-    </div>
-
-</section>
- 
-
-</footer>
+          </div>
+        </section>
+      </footer>
     </div>
   );
 };
